5968e7009970621bb5e8301ab7c2f5852fd6521b
mramasoud <Amirhoseinmasoud99@gmail.com>
2023-12-23 23:16:47 +0330

    add AppointmentRepository , PatientRepository , DoctorRepository

---
+++ src/main/java/com/blubank/doctorappointment/repository/AppointmentRepository.java
@@ -0,0 +1,12 @@
+package com.blubank.doctorappointment.repository;
+
+import com.blubank.doctorappointment.entity.Appointment;
+import com.blubank.doctorappointment.entity.Doctor;
+import com.blubank.doctorappointment.enumbration.AppointmentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment,Long>{
+}
