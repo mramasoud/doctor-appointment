35603da2236e39bea86c1a2fe9702c4e8007b31e
A.Masoud <a.masoud@shaparak.com>
2023-12-24 11:23:38 +0330

    refactor doctor service and doctor controller

--- src/main/java/com/blubank/doctorappointment/service/DoctorService.java
+++ src/main/java/com/blubank/doctorappointment/service/DoctorService.java
@@ -5,92 +5,67 @@ import com.blubank.doctorappointment.dto.DoctorDTO;
 import com.blubank.doctorappointment.entity.Appointment;
 import com.blubank.doctorappointment.entity.Doctor;
 import com.blubank.doctorappointment.enumbration.AppointmentStatus;
+import com.blubank.doctorappointment.enumbration.DoctorCodeProjectEnum;
 import com.blubank.doctorappointment.repository.AppointmentRepository;
 import com.blubank.doctorappointment.repository.DoctorRepository;
+import com.blubank.doctorappointment.response.Response;
+import com.blubank.doctorappointment.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
-public class DoctorService {
+public class DoctorService{
     @Autowired
-    private  DoctorRepository doctorRepository;
+    private DoctorRepository doctorRepository;
     @Autowired
     private AppointmentRepository appointmentRepository;
-
+    private static final int timePeriods_Min = 30;
 
     public ResponseEntity<String> addDoctor(DoctorDTO dto){
         Doctor doctor = doctorRepository.save(new Doctor(dto.getName()));
-        if(doctor.getDoctors_Id()!=null){
-            return new ResponseEntity<>("appointment has saved.", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Time in day not valid, please enter time between 00:00 and 23:59", HttpStatus.BAD_REQUEST);
+        if(doctor.getDoctors_Id() != null){
+            return new ResponseEntity<>("appointment has saved." , HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Time in day not valid, please enter time between 00:00 and 23:59" , HttpStatus.BAD_REQUEST);
         }
-    }
-    public ResponseEntity<String> addDoctorAvailabilityTime(DoctorAvailabilityDTO dto) {
-        if (dateTimeIsValid(dto.getStartTime(), dto.getEndTime())) {
-            List<DoctorAvailabilityDTO> availableTimePeriods = getAvailableTimePeriods(dto.getDayOfMonth(),dto.getStartTime(), dto.getEndTime());
-            System.out.println("time in activeTime is" + availableTimePeriods.size());
-            if (availableTimePeriods.size() != 0) {
-                try {
-                    for (DoctorAvailabilityDTO time: availableTimePeriods) {
-                        saveDoctorAvailability(time);
-                    }
-                    return new ResponseEntity<>("appointment has saved.", HttpStatus.OK);
-                } catch (RuntimeException exception) {
-                    return new ResponseEntity<>("Error server ", HttpStatus.INTERNAL_SERVER_ERROR);
-                } catch (Exception exception) {
-                    return new ResponseEntity<>("Error saving availability", HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            } else {
-                return new ResponseEntity<>("Time in day not valid, please enter time between 00:00 and 23:59", HttpStatus.BAD_REQUEST);
-            }
-        } else {
-            return new ResponseEntity<>("Time in day not valid, please enter time between 00:00 and 23:59", HttpStatus.BAD_REQUEST);
-        }
-    }
-    private void saveDoctorAvailability(DoctorAvailabilityDTO dto) {
-        appointmentRepository.save(dataProvider(dto));
-    }
-    public Appointment dataProvider(DoctorAvailabilityDTO dto) {
-        Appointment appointment = new Appointment();
-        ZonedDateTime startDateTime = ZonedDateTime.now(ZoneId.systemDefault()).with(dto.getStartTime());
-        ZonedDateTime endDateTime = ZonedDateTime.now(ZoneId.systemDefault()).with(dto.getEndTime());
-        appointment.setStartTime(Date.from(startDateTime.toInstant()));
-        appointment.setEndTime(Date.from(endDateTime.toInstant()));
-        appointment.setStatus(AppointmentStatus.empty);
-        appointment.setDayOfMonth(dto.getDayOfMonth());
-        appointment.setDoctor(doctorRepository.findByName(dto.getDoctorName()));
-        return appointment;
-    }
-    public static int calculateAvailableSlots(LocalTime startTime, LocalTime endTime) {
-       return (int) ChronoUnit.MINUTES.between(startTime, endTime)/30;
     }
 
-    public static boolean dateTimeIsValid(LocalTime startTime, LocalTime endTime) {
-        return startTime.isAfter(LocalTime.of(0, 0)) && endTime.isBefore(LocalTime.of(23, 59));
+    public List<Response> addDoctorAvailableTimes(DoctorAvailabilityDTO dto , List<Response> responses){
+        Doctor doctor = doctorRepository.findByName(dto.getDoctorName());
+        if(doctor == null){
+            responses.add(new Response(DoctorCodeProjectEnum.doctorNotFound.getErrorCode() , DoctorCodeProjectEnum.doctorNotFound.getErrorDescription()));
+            return responses;
+        }
+        try{
+            saveDoctorAvailableTime(getAvailableTimePeriods(dto.getDayOfMonth() , dto.getStartTime() , dto.getEndTime() , doctor));
+            responses.add(new Response(DoctorCodeProjectEnum.appointmentSaved.getErrorCode() , DoctorCodeProjectEnum.appointmentSaved.getErrorDescription()));
+            return responses;
+        }catch(Exception exception){
+            responses.add(new Response(DoctorCodeProjectEnum.serverError.getErrorCode() , DoctorCodeProjectEnum.serverError.getErrorDescription()));
+            return responses;
+        }
     }
-    public static List<DoctorAvailabilityDTO> getAvailableTimePeriods(int dayOfMonth,LocalTime startTime, LocalTime endTime) {
-        List<DoctorAvailabilityDTO> timePeriods = new ArrayList<>();
+
+
+    private void saveDoctorAvailableTime(List<Appointment> availableTimePeriods){
+        appointmentRepository.saveAll(availableTimePeriods);
+    }
+
+    public static List<Appointment> getAvailableTimePeriods(int dayOfMonth , LocalTime startTime , LocalTime endTime , Doctor doctor){
+        List<Appointment> timePeriods = new ArrayList<>();
         LocalTime current = startTime;
 
-        while (current.isBefore(endTime)) {
-
-            LocalTime next = current.plusMinutes(30);
-            if (next.isAfter(endTime) || next.equals(endTime)) {
-               break;
-            } else {
-                timePeriods.add(new DoctorAvailabilityDTO(dayOfMonth,current,next));
-            }
+        while(current.isBefore(endTime)){
+            LocalTime next = current.plusMinutes(timePeriods_Min);
+            if(next.isAfter(endTime))
+                break;
+            timePeriods.add(new Appointment(DateUtil.dateConvertor(current) , DateUtil.dateConvertor(endTime) , dayOfMonth , AppointmentStatus.empty , doctor));
             current = next;
         }
         return timePeriods;
--- src/main/java/com/blubank/doctorappointment/validation/DoctorValidationService.java
+++ src/main/java/com/blubank/doctorappointment/validation/DoctorValidationService.java
@@ -1,6 +1,7 @@
 package com.blubank.doctorappointment.validation;
 
 import com.blubank.doctorappointment.dto.DoctorAvailabilityDTO;
+import com.blubank.doctorappointment.enumbration.DateTimeErrorCodeEnum;
 import com.blubank.doctorappointment.response.Response;
 import com.blubank.doctorappointment.util.DateUtil;
 import org.springframework.stereotype.Service;
@@ -9,23 +10,16 @@ import java.util.List;
 
 @Service
 public class DoctorValidationService implements ValidationService<DoctorAvailabilityDTO>{
-
-    private static final int dateNotValidCode = 101;
-    private static final String dateNotValidMessage = "day of month not valid ,please enter day between 1 and 31 ";
-    private static final int timeNotValidCode = 102;
-    private static final String timeNotValidMessage = "Time in day not valid, please enter time between 00:00 and 23:59";
-    private static final String startTimeNotValidMessage = "start time is not valid, please enter time between 00:00 and 23:59";
-    private static final String endTimeNotValidMessage = "end time is not valid, please enter time between 00:00 and 23:59";
     @Override
     public boolean validate(DoctorAvailabilityDTO dto , List<Response> responses){
         if(! DateUtil.dateTimeIsValid(dto.getStartTime()))
-            responses.add(new Response(timeNotValidCode , startTimeNotValidMessage));
+            responses.add(new Response(DateTimeErrorCodeEnum.startTimeNotValid.getErrorCode() , DateTimeErrorCodeEnum.startTimeNotValid.getErrorDescription()));
         if(! DateUtil.dateTimeIsValid(dto.getEndTime()))
-            responses.add(new Response(timeNotValidCode , endTimeNotValidMessage));
+            responses.add(new Response(DateTimeErrorCodeEnum.endTimeNotValid.getErrorCode() , DateTimeErrorCodeEnum.endTimeNotValid.getErrorDescription()));
         if(! DateUtil.dateTimeIsValid(dto.getStartTime() , dto.getEndTime()))
-            responses.add(new Response(timeNotValidCode , timeNotValidMessage));
+            responses.add(new Response(DateTimeErrorCodeEnum.timeNotValid.getErrorCode() , DateTimeErrorCodeEnum.endTimeNotValid.getErrorDescription()));
         if(! DateUtil.dayOfMonthValidation(dto.getDayOfMonth()))
-            responses.add(new Response(dateNotValidCode , dateNotValidMessage));
+            responses.add(new Response(DateTimeErrorCodeEnum.dateNotValid.getErrorCode() ,DateTimeErrorCodeEnum.dateNotValid.getErrorDescription()));
         return responses.size() == 0;
     }
 }
